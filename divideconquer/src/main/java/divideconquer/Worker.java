cbbe6fd76bfe7f24a29bf685543cfdf60c630ff3
Andrias Meisyal <andriasonline@gmail.com>
2015-05-22 21:02:07 +0700

    update some parts

--- divideconquer/src/main/java/divideconquer/Worker.java
+++ divideconquer/src/main/java/divideconquer/Worker.java
@@ -2,20 +2,10 @@ package divideconquer;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-import java.rmi.RemoteException;
 
 import javax.imageio.ImageIO;
-
-import ij.IJ;
-import ij.ImagePlus;
-import ij.process.ImageConverter;
 
 import org.zeromq.ZMQ;
 
@@ -29,38 +19,42 @@ public class Worker {
 		ZMQ.Socket sender = context.socket(ZMQ.PUSH);
 		sender.connect("tcp://localhost:5558");
 
-
 		// Process image
-		while (!Thread.currentThread().isInterrupted()) {
-			ByteBuffer recData = ByteBuffer.allocate(4096);
-			int buffer = 0;
-			byte[] tmpData ;
-			OutputStream out = null;
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		while (!Thread.currentThread().isInterrupted()) {;
+			byte[] imageByteArray;
+			
+			// Receive data from ventilator
+			String fileName = receiver.recvStr();
+			String fileExtensions = receiver.recvStr();
+			imageByteArray = receiver.recv();
+			
+			System.out.println(imageByteArray.length);
 		
-			tmpData = receiver.recv();
-			System.out.println(tmpData.length);
-		
-			BufferedImage img = ImageIO.read(new ByteArrayInputStream(tmpData));
-			int x, y;
-			int height = img.getHeight();
-			int width = img.getWidth();
-			BufferedImage imgOut = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-
-			for (y = 0; y < height; y++) {
-				for (x = 0; x < width; x++) {
-	
-					Color c = new Color(img.getRGB(x, y));
-					int temp = (int) (c.getRed() * 0.299 + c.getGreen() * 0.587 + c
-							.getBlue() * 0.114);
-					imgOut.setRGB(x, y, temp << 16 | temp << 8 | temp);
+			BufferedImage imageIn = ImageIO.read(new ByteArrayInputStream(imageByteArray));
+			
+			// Get image attributes
+			int height = imageIn.getHeight();
+			int width = imageIn.getWidth();
+			
+			BufferedImage imageAttributes = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+			
+			// Convert to grayscale
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					Color c = new Color(imageIn.getRGB(x, y));
+					int temp = (int) (c.getRed() * 0.299 + c.getGreen() * 0.587 + c.getBlue() * 0.114);
+					imageAttributes.setRGB(x, y, temp << 16 | temp << 8 | temp);
 				}
 			}
 			
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(imgOut, "png", baos);
-			byte[] sendImg = baos.toByteArray();
-            sender.send(sendImg);
+			ImageIO.write(imageAttributes, fileExtensions, baos);
+			
+			byte[] imageOut = baos.toByteArray();
+            
+			sender.send(fileName);
+			sender.send(fileExtensions);
+			sender.send(imageOut);
 		}
 
 		sender.close();
